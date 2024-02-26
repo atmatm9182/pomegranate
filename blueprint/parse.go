8b51f336eb7b600e0b79f7a447f45b13f46f48ca
atmatm9182 <atmatm9182@gmail.com>
2024-02-26 19:53:22 +0100

    Changed 'blueprint.Parse' function to handle directories

--- blueprint/parse.go
+++ blueprint/parse.go
@@ -10,8 +10,28 @@ import (
 const DefaultBlueprintPath = "pomegranate.yml"
 
 func Parse(configPath string) (blueprint Blueprint, err error) {
+	var configFile *os.File
+	configFile, err = os.Open(configPath)
+	if err != nil {
+		return
+	}
+	defer configFile.Close()
+
+	var stat os.FileInfo
+	stat, err = configFile.Stat()
+	if err != nil {
+		return
+	}
+	
     var configData []byte
-    configData, err = os.ReadFile(configPath)
+	if stat.IsDir() {
+		configPath = path.Join(configPath, DefaultBlueprintPath)
+		configData, err = os.ReadFile(configPath)
+	} else {
+		configData = make([]byte, 0, stat.Size())
+		_, err = configFile.Read(configData)
+	}
+
     if err != nil {
         return
     }
